00ea8c3fee45a7bf6a0ec232a6c61a9925abe55a
user <user@mail.com>
2016-03-21 11:39:43 +0800

    program.cs test

---
+++ Test/EasyPerformanceCountersHelper.Test/Program.cs
@@ -0,0 +1,63 @@
+﻿namespace Test
+{
+    using Microshaoft;
+    using System;
+    //using System;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using System.Diagnostics;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var category = "Microshaoft-Test-Category-002";
+            var instance = "Instance-002";
+            //var enabledCountPerformance = false;
+            CommonPerformanceCountersContainer container = null;
+            EasyPerformanceCountersHelper<CommonPerformanceCountersContainer>
+                    .AttachPerformanceCountersCategoryInstance
+                        (
+                            category
+                            , instance
+                            , out container
+                            , PerformanceCounterInstanceLifetime.Process
+                            //, initializePerformanceCounterInstanceRawValue: 1009
+                        );
+            Parallel
+                    .For
+                        (
+                            0
+                            , 5000
+                            , new ParallelOptions() {  MaxDegreeOfParallelism = 32 }
+                            , (x) =>
+                            {
+                                EasyPerformanceCountersHelper
+                                        <CommonPerformanceCountersContainer>
+                                            .TryCountPerformance
+                                                (
+                                                    PerformanceCounterProcessingFlagsType
+                                                                    .All
+                                                                    //.NonTimeBased
+                                                                    //.TimeBasedOnBeginOnEnd
+                                                    //MultiPerformanceCountersTypeFlags.ProcessAllCounters
+                                                    , category
+                                                    , instance
+                                                    , () => true
+                                                    //, null
+                                                    , () =>
+                                                    {
+                                                        //Thread.Sleep(1);
+                                                        //throw new Exception();
+                                                    }
+                                                    , (xx, yy) =>
+                                                    {
+                                                        //Console.WriteLine("on {0}, counterName {1} ", xx, yy.CounterName);
+                                                        return 1;
+                                                    }
+                                                );
+                            }
+                        );
+            Console.ReadLine();
+        }
+    }
+}
