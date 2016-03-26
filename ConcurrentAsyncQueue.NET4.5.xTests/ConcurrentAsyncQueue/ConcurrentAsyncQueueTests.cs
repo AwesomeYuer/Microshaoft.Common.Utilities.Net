1062fff35f598109124ca80e5eb4a40c369a3a3c
user <user@mail.com>
2016-03-26 17:53:12 +0800

    UnitTests

---
+++ ConcurrentAsyncQueue.NET4.5.xTests/ConcurrentAsyncQueue/ConcurrentAsyncQueueTests.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microshaoft;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microshaoft.Tests
+{
+    [TestClass()]
+    public class ConcurrentAsyncQueueTests
+    {
+        [TestMethod()]
+        public void ConcurrentAsyncQueueTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ClearPerformanceCountersValuesTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetPerformanceCounterByNameTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetPerformanceCountersByLevelTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void AttachPerformanceCountersTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void StartDecreaseDequeueProcessThreadsTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void StartIncreaseDequeueProcessThreadsTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void EnqueueTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
