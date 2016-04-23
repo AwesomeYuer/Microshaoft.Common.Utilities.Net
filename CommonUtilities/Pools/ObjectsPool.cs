45e16809d3066d8d59833259ca9cf9cb1094304d
user <user@mail.com>
2016-04-24 02:51:18 +0800

    PoolingObjectsLoadingMode

---
+++ CommonUtilities/Pools/ObjectsPool.cs
@@ -0,0 +1,290 @@
+﻿
+namespace Microshaoft
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.Threading;
+
+    public enum PoolingObjectsLoadingMode { Eager, Lazy, LazyExpanding };
+
+    public enum PoolingObjectsAccessMode { FIFO, LIFO, Circular };
+
+    public class ObjectsPool<T> : IDisposable
+    {
+        private bool isDisposed;
+        private Func<ObjectsPool<T>, T> factory;
+        private PoolingObjectsLoadingMode loadingMode;
+        private IItemStore itemStore;
+        private int size;
+        private int count;
+        private Semaphore sync;
+
+        public ObjectsPool(int size, Func<ObjectsPool<T>, T> factory)
+            : this(size, factory, PoolingObjectsLoadingMode.Lazy, PoolingObjectsAccessMode.FIFO)
+        {
+        }
+
+        public ObjectsPool(int size, Func<ObjectsPool<T>, T> factory,
+            PoolingObjectsLoadingMode loadingMode, PoolingObjectsAccessMode accessMode)
+        {
+            if (size <= 0)
+                throw new ArgumentOutOfRangeException("size", size,
+                    "Argument 'size' must be greater than zero.");
+            if (factory == null)
+                throw new ArgumentNullException("factory");
+
+            this.size = size;
+            this.factory = factory;
+            sync = new Semaphore(size, size);
+            this.loadingMode = loadingMode;
+            this.itemStore = CreateItemStore(accessMode, size);
+            if (loadingMode == PoolingObjectsLoadingMode.Eager)
+            {
+                PreloadItems();
+            }
+        }
+
+        public T Acquire()
+        {
+            sync.WaitOne();
+            switch (loadingMode)
+            {
+                case PoolingObjectsLoadingMode.Eager:
+                    return AcquireEager();
+                case PoolingObjectsLoadingMode.Lazy:
+                    return AcquireLazy();
+                default:
+                    Debug.Assert(loadingMode == PoolingObjectsLoadingMode.LazyExpanding,
+                        "Unknown LoadingMode encountered in Acquire method.");
+                    return AcquireLazyExpanding();
+            }
+        }
+
+        public void Release(T item)
+        {
+            lock (itemStore)
+            {
+                itemStore.Store(item);
+            }
+            sync.Release();
+        }
+
+        public void Dispose()
+        {
+            if (isDisposed)
+            {
+                return;
+            }
+            isDisposed = true;
+            if (typeof(IDisposable).IsAssignableFrom(typeof(T)))
+            {
+                lock (itemStore)
+                {
+                    while (itemStore.Count > 0)
+                    {
+                        IDisposable disposable = (IDisposable)itemStore.Fetch();
+                        disposable.Dispose();
+                    }
+                }
+            }
+            sync.Close();
+        }
+
+        #region Acquisition
+
+        private T AcquireEager()
+        {
+            lock (itemStore)
+            {
+                return itemStore.Fetch();
+            }
+        }
+
+        private T AcquireLazy()
+        {
+            lock (itemStore)
+            {
+                if (itemStore.Count > 0)
+                {
+                    return itemStore.Fetch();
+                }
+            }
+            Interlocked.Increment(ref count);
+            return factory(this);
+        }
+
+        private T AcquireLazyExpanding()
+        {
+            bool shouldExpand = false;
+            if (count < size)
+            {
+                int newCount = Interlocked.Increment(ref count);
+                if (newCount <= size)
+                {
+                    shouldExpand = true;
+                }
+                else
+                {
+                    // Another thread took the last spot - use the store instead
+                    Interlocked.Decrement(ref count);
+                }
+            }
+            if (shouldExpand)
+            {
+                return factory(this);
+            }
+            else
+            {
+                lock (itemStore)
+                {
+                    return itemStore.Fetch();
+                }
+            }
+        }
+
+        private void PreloadItems()
+        {
+            for (int i = 0; i < size; i++)
+            {
+                T item = factory(this);
+                itemStore.Store(item);
+            }
+            count = size;
+        }
+
+        #endregion
+
+        #region Collection Wrappers
+
+        interface IItemStore
+        {
+            T Fetch();
+            void Store(T item);
+            int Count { get; }
+        }
+
+        private IItemStore CreateItemStore(PoolingObjectsAccessMode mode, int capacity)
+        {
+            switch (mode)
+            {
+                case PoolingObjectsAccessMode.FIFO:
+                    return new QueueStore(capacity);
+                case PoolingObjectsAccessMode.LIFO:
+                    return new StackStore(capacity);
+                default:
+                    Debug.Assert(mode == PoolingObjectsAccessMode.Circular,
+                        "Invalid AccessMode in CreateItemStore");
+                    return new CircularStore(capacity);
+            }
+        }
+
+        class QueueStore : Queue<T>, IItemStore
+        {
+            public QueueStore(int capacity)
+                : base(capacity)
+            {
+            }
+
+            public T Fetch()
+            {
+                return Dequeue();
+            }
+
+            public void Store(T item)
+            {
+                Enqueue(item);
+            }
+        }
+
+        class StackStore : Stack<T>, IItemStore
+        {
+            public StackStore(int capacity)
+                : base(capacity)
+            {
+            }
+
+            public T Fetch()
+            {
+                return Pop();
+            }
+
+            public void Store(T item)
+            {
+                Push(item);
+            }
+        }
+
+        class CircularStore : IItemStore
+        {
+            private List<Slot> slots;
+            private int freeSlotCount;
+            private int position = -1;
+
+            public CircularStore(int capacity)
+            {
+                slots = new List<Slot>(capacity);
+            }
+
+            public T Fetch()
+            {
+                if (Count == 0)
+                    throw new InvalidOperationException("The buffer is empty.");
+
+                int startPosition = position;
+                do
+                {
+                    Advance();
+                    Slot slot = slots[position];
+                    if (!slot.IsInUse)
+                    {
+                        slot.IsInUse = true;
+                        --freeSlotCount;
+                        return slot.Item;
+                    }
+                } while (startPosition != position);
+                throw new InvalidOperationException("No free slots.");
+            }
+
+            public void Store(T item)
+            {
+                Slot slot = slots.Find(s => object.Equals(s.Item, item));
+                if (slot == null)
+                {
+                    slot = new Slot(item);
+                    slots.Add(slot);
+                }
+                slot.IsInUse = false;
+                ++freeSlotCount;
+            }
+
+            public int Count
+            {
+                get { return freeSlotCount; }
+            }
+
+            private void Advance()
+            {
+                position = (position + 1) % slots.Count;
+            }
+
+            class Slot
+            {
+                public Slot(T item)
+                {
+                    this.Item = item;
+                }
+
+                public T Item { get; private set; }
+                public bool IsInUse { get; set; }
+            }
+        }
+
+        #endregion
+
+        public bool IsDisposed
+        {
+            get { return isDisposed; }
+        }
+    }
+}
