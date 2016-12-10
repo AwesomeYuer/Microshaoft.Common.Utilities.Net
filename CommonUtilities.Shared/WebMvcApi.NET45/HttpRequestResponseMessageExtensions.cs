e699a7ab52eacc5c2b2626e59073c4510081eb79
user <user@mail.com>
2016-12-10 11:11:22 +0800

    HttpRequestResponseMessageExtensions

---
+++ CommonUtilities.Shared/WebMvcApi.NET45/HttpRequestResponseMessageExtensions.cs
@@ -0,0 +1,62 @@
+﻿namespace Microshaoft.Web
+{
+    using System.Net.Http;
+    public static class HttpRequestMessageExtensions
+    {
+        public static bool TryGetPropertyValue<T>
+                                (
+                                    this HttpRequestMessage target
+                                    , string propertyKey
+                                    , out T propertyValue
+                                )
+        {
+            var r = false;
+            object propertyValueObject = null;
+            propertyValue = default(T);
+            r =
+                (
+                    target
+                    .Properties
+                    .TryGetValue
+                        (
+                            propertyKey
+                            , out propertyValueObject
+                        )
+                );
+            if (r)
+            { 
+                propertyValue = (T) propertyValueObject;
+            }
+            return r;
+        }
+
+        public static T GetPropertyValue<T>
+                        (
+                            this HttpRequestMessage target
+                            , string propertyKey
+                        )
+        {
+            var r = false;
+            object propertyValueObject = null;
+            var propertyValue = default(T);
+            r =
+                (
+                    target
+                        .Properties
+                        .TryGetValue
+                            (
+                                propertyKey
+                                , out propertyValueObject
+                            )
+                );
+            if (r)
+            {
+                propertyValue = (T)propertyValueObject;
+            }
+            return propertyValue;
+        }
+
+
+       
+    }
+}
