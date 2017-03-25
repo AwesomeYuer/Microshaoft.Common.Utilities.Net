2eb51739370126124bbee8670882aa4b4829faac
user <user@mail.com>
2017-03-25 17:59:13 +0800

    CommonUtilities.SharedProjects.Sln/Projects rename using Framework, prepare diff with multiple platform Standard, Core Seperate

---
+++ CommonUtilities/EasyPerformanceCountersHelper/EasyPerformanceCountersHelper.NET.Framework.4.5.x/Properties/AssemblyInfo.cs
@@ -0,0 +1,25 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using Microshaoft.Net;
+
+[assembly: AssemblyDescription(AssemblyInfoManager.AssemblyDescription)]
+
+namespace Microshaoft.Net
+{
+    using System;
+    using System.Reflection;
+    public static class AssemblyInfoManager
+    {
+
+        public const string AssemblyDescription =
+#if NET35
+            "for .NET 3.5"
+#elif NET45
+            "for .NET 4.5.1"
+#else
+            "Microshaoft.Net for .NET"
+#endif
+;
+    }
+}
