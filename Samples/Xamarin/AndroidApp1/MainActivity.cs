6cf0b88fe8d82c6bf4fd2818f4ff03931fd03810
user <user@mail.com>
2018-03-05 17:27:48 +0800

    add Xamarin/.NET Standard 2.0 support . build/Compile ok for android

---
+++ Samples/Xamarin/AndroidApp1/MainActivity.cs
@@ -0,0 +1,22 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+
+
+
+namespace AndroidApp1
+{
+    [Activity(Label = "AndroidApp1", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+        }
+    }
+}
+
