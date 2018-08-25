949382c09d9fbe7acc354ab209276ed46628fc16
user <user@mail.com>
2018-08-25 22:33:34 +0800

    MsSqlCodeDiffVersioning.WebApplication

---
+++ Samples/MsSqlCodeDiffVersioning/Controllers/StoreProcedureExecutorController.cs
@@ -0,0 +1,22 @@
+﻿#if !NETFRAMEWORK4_X && !NETSTANDARD2_0
+namespace Microshaoft.WebApi.Controllers
+{
+    using Microshaoft.Web;
+    using Microsoft.AspNetCore.Cors;
+    using Microsoft.AspNetCore.Mvc;
+    [Route("api/[controller]")]
+    [ApiController]
+    [EnableCors("AllowAllOrigins")]
+    public class StoreProcedureExecutorController
+                    : AbstractStoreProceduresExecutorControllerBase
+    {
+        /*
+         * http://localhost:5816/api/StoreProcedureExecutor/mssql1/zsp_test?{datetime:"2019-01-01",pBIT:true,pBOOL:1,pTINYINT:1,pSMALLINT:16,pMEDIUMINT:25,pINT:65536,pBIGINT:999999,pFLOAT:9999.99,pDOUBLE:9999.99,pDECIMAL:9999.99,pCHAR:"a",pVARCHAR:"aaaaaaaaaa",pDate:"2018-09-01",pDateTime:"2018-09-01 21:00:10",pTimeStamp:null,pTime:null,pYear:null,udt_vcidt:[{varchar:"aaaa",date:"2018-11-11",int:789},{varchar:"bbbb",date:"2018-11-12",int:123}]}
+         */
+        public StoreProcedureExecutorController(IStoreProceduresWebApiService service)
+                : base(service)
+        {
+        }
+    }
+}
+#endif
---
+++ Samples/MsSqlCodeDiffVersioning/Services/StoreProcedureExecuteService.cs
@@ -0,0 +1,27 @@
+﻿namespace Microshaoft.Web
+{
+    public class StoreProceduresExecuteService
+                            : AbstractStoreProceduresService
+    {
+        //called by base AbstractStoreProceduresService constructor
+        public override void Initialize()
+        {
+            //using derived class implmention
+            LoadDataBasesConnectionsInfo("dbConnections.json");
+            //using base class implmention
+            base
+                .LoadDynamicExecutors("dynamicLoadExecutorsPaths.json");
+        }
+        protected override void LoadDataBasesConnectionsInfo(string dbConnectionsJsonFile = "dbConnections.json")
+        {
+            // test for override base LoadDataBasesConnectionsInfo implement
+            // you can implement by using other process except config json file(that's base method)
+            base
+                .LoadDataBasesConnectionsInfo(dbConnectionsJsonFile);
+        }
+        protected override int
+                CachedParametersDefinitionExpiredInSeconds => 11;
+        protected override bool 
+                NeedAutoRefreshExecutedTimeForSlideExpire => true;
+    }
+}
