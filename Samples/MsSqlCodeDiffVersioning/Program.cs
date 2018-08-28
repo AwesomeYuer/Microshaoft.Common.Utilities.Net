5a7c25db01a2fdb68510851cd8963b5e1ce8197a
user <user@mail.com>
2018-08-28 16:30:43 +0800

    Os Platform runtime information

--- Samples/MsSqlCodeDiffVersioning/Program.cs
+++ Samples/MsSqlCodeDiffVersioning/Program.cs
@@ -1,14 +1,53 @@
 ﻿namespace WebApplication.ASPNetCore
 {
+    using Microshaoft;
     using Microsoft.AspNetCore;
     using Microsoft.AspNetCore.Hosting;
     using Microsoft.Extensions.Configuration;
+    using System;
     using System.IO;
+    using System.Linq;
+    using System.Runtime.InteropServices;
 
     public class Program
-    {
+    { 
         public static void Main(string[] args)
         {
+            OSPlatform OSPlatform
+                    = EnumerableHelper
+                            .Range
+                                (
+                                    OSPlatform.Linux
+                                    , OSPlatform.OSX
+                                    , OSPlatform.Windows
+                                )
+                            .First
+                                (
+                                    (x) =>
+                                    {
+                                        return
+                                            RuntimeInformation
+                                                .IsOSPlatform(x);
+                                    }
+                                );
+            var s = $"{nameof(RuntimeInformation.FrameworkDescription)}:{RuntimeInformation.FrameworkDescription}";
+            s += "\n";
+            s += $"{nameof(RuntimeInformation.OSArchitecture)}:{RuntimeInformation.OSArchitecture.ToString()}";
+            s += "\n";
+            s += $"{nameof(RuntimeInformation.OSDescription)}:{RuntimeInformation.OSDescription}";
+            s += "\n";
+            s += $"{nameof(RuntimeInformation.ProcessArchitecture)}:{RuntimeInformation.ProcessArchitecture.ToString()}";
+            s += "\n";
+            s += $"{nameof(OSPlatform)}:{OSPlatform}";
+            Console.WriteLine(s);
+            var os = Environment.OSVersion;
+            Console.WriteLine("Current OS Information:\n");
+            Console.WriteLine("Platform: {0:G}", os.Platform);
+            Console.WriteLine("Version String: {0}", os.VersionString);
+            Console.WriteLine("Version Information:");
+            Console.WriteLine("   Major: {0}", os.Version.Major);
+            Console.WriteLine("   Minor: {0}", os.Version.Minor);
+            Console.WriteLine("Service Pack: '{0}'", os.ServicePack);
             CreateWebHostBuilder
                 (args)
                     //.UseKestrel()
@@ -35,6 +74,5 @@
                     //.UseUrls("http://+:5000")
                     .UseStartup<Startup>();
         }
-        
     }
 }
