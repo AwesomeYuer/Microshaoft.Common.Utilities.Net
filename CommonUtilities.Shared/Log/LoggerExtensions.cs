d32777134faf2edfeaa8a675e238b56ee0cd1a65
user <user@mail.com>
2018-11-13 17:25:29 +0800

    LoggerExtensions LogOnDemand

---
+++ CommonUtilities.Shared/Log/LoggerExtensions.cs
@@ -0,0 +1,150 @@
+﻿#if !NETFRAMEWORK4_X
+namespace Microshaoft
+{
+    using Microsoft.Extensions.Logging;
+    using System;
+    public static class LoggerExtensions
+    {
+        public static void LogOnDemand
+                        (
+                            this ILogger target
+                            , LogLevel logLevel
+                            , Func
+                                <
+                                    (
+                                        EventId LoggingEventId
+                                        , Exception LoggingException
+                                        , string LoggingMessage
+                                        , object[] LoggingArguments
+                                    )
+                                > process
+                        )
+        {
+            if (target.IsEnabled(logLevel))
+            {
+                var r = process();
+                target
+                    .Log
+                        (
+                            logLevel
+                            , r.LoggingEventId
+                            , r.LoggingException
+                            , r.LoggingMessage
+                            , r.LoggingArguments
+                        );
+            }
+        }
+        public static void LogOnDemand
+                        (
+                            this ILogger target
+                            , LogLevel logLevel
+                            , Func
+                                <
+                                    (
+                                        EventId LoggingEventId
+                                        , string LoggingMessage
+                                        , object[] LoggingArguments
+                                    )
+                                > process
+                        )
+        {
+            if (target.IsEnabled(logLevel))
+            {
+                var r = process();
+                target
+                    .Log
+                        (
+                            logLevel
+                            , r.LoggingEventId
+                            //, r.LoggingException
+                            , r.LoggingMessage
+                            , r.LoggingArguments
+                        );
+            }
+        }
+        public static void LogOnDemand
+                        (
+                            this ILogger target
+                            , LogLevel logLevel
+                            , Func
+                                <
+                                    (
+                                        string LoggingMessage
+                                        , object[] LoggingArguments
+                                    )
+                                > process
+                        )
+        {
+            if (target.IsEnabled(logLevel))
+            {
+                var r = process();
+                target
+                    .Log
+                        (
+                            logLevel
+                            , r.LoggingMessage
+                            , r.LoggingArguments
+                        );
+            }
+        }
+
+        public static void LogOnDemand
+                (
+                    this ILogger target
+                    , LogLevel logLevel
+                    , Func
+                        <
+                            (
+                                Exception LoggingException
+                                , string LoggingMessage
+                                , object[] LoggingArguments
+                            )
+                        > process
+                )
+        {
+            if (target.IsEnabled(logLevel))
+            {
+                var r = process();
+                target
+                    .Log
+                        (
+                            logLevel
+                            , r.LoggingException
+                            , r.LoggingMessage
+                            , r.LoggingArguments
+                        );
+            }
+        }
+
+        public static void LogOnDemand<TState>
+                (
+                    this ILogger target
+                    , LogLevel logLevel
+                    , Func
+                        <
+                            (
+                                EventId LoggingEventId
+                                , Exception LoggingException
+                                , TState State
+                            )
+                        > process
+                    , Func<TState, Exception, string> formatter
+                )
+        {
+            if (target.IsEnabled(logLevel))
+            {
+                var r = process();
+                target
+                    .Log<TState>
+                        (
+                            logLevel
+                            , r.LoggingEventId
+                            , r.State
+                            , r.LoggingException
+                            , formatter
+                        );
+            }
+        }
+    }
+}
+#endif
