5e7b73f3a7993b05b97a776ab6bacb6223ecc69e
user <user@mail.com>
2018-12-24 16:39:09 +0800

    JsonReaderHelper / JsonHelper

---
+++ CommonUtilities.Shared/Json/JsonHelper/JsonHelper.04.cs
@@ -0,0 +1,58 @@
+﻿#if NETCOREAPP2_X
+namespace Microshaoft
+{
+    using Newtonsoft.Json;
+    using System.Buffers;
+    using System.IO;
+    public static partial class JsonHelper
+    {
+        public static JsonTextReader CreateJsonTextReader(this TextReader target)
+        {
+            var reader = new JsonTextReader(target);
+            reader.ArrayPool = JsonArrayPool<char>.Shared;
+
+            // Don't close the input, leave closing to the caller
+            reader.CloseInput = false;
+
+            return reader;
+        }
+
+        public static JsonTextWriter CreateJsonTextWriter(this TextWriter target)
+        {
+            var writer = new JsonTextWriter(target);
+            writer.ArrayPool = JsonArrayPool<char>.Shared;
+            // Don't close the output, leave closing to the caller
+            writer.CloseOutput = false;
+
+            // SignalR will always write a complete JSON response
+            // This setting will prevent an error during writing be hidden by another error writing on dispose
+            writer.AutoCompleteOnClose = false;
+
+            return writer;
+        }
+
+
+        private class JsonArrayPool<T> : IArrayPool<T>
+        {
+            private readonly ArrayPool<T> _inner;
+
+            internal static readonly JsonArrayPool<T> Shared = new JsonArrayPool<T>(ArrayPool<T>.Shared);
+
+            public JsonArrayPool(ArrayPool<T> inner)
+            {
+                _inner = inner;
+            }
+
+            public T[] Rent(int minimumLength)
+            {
+                return _inner.Rent(minimumLength);
+            }
+
+            public void Return(T[] array)
+            {
+                _inner.Return(array);
+            }
+        }
+    }
+}
+#endif
