65f8c5dc76bd53d9d8d1eb2809d6e9f090a5c2f1
user <user@mail.com>
2018-12-26 11:17:14 +0800

    RequestResponseGuardMiddleware

--- CommonUtilities.Shared/Web.MiddleWares/RequestResponseGuardMiddleware.cs
+++ CommonUtilities.Shared/Web.MiddleWares/RequestResponseGuardMiddleware.cs
@@ -3,22 +3,19 @@ namespace Microshaoft.Web
 {
     using Microsoft.AspNetCore.Builder;
     using Microsoft.AspNetCore.Http;
-    using System;
     using System.Diagnostics;
     using System.Threading.Tasks;
     public class RequestResponseGuardMiddleware
     {
         private readonly RequestDelegate _next;
-
         public RequestResponseGuardMiddleware(RequestDelegate next)
         {
             _next = next;
         }
-
         public async Task Invoke(HttpContext context)
         {
             var stopwatch = new Stopwatch();
-            Console.WriteLine("Request begin ...");
+            //Console.WriteLine("Request begin ...");
             stopwatch.Start();
             context
                 .Response
@@ -30,24 +27,28 @@ namespace Microshaoft.Web
                             var duration = stopwatch.ElapsedMilliseconds;
                             context
                                 .Response
-                                .Headers["x-Response-Time"] = duration.ToString();
-                            Console.WriteLine($"Response end {duration}!!!");
+                                .Headers["X-Request-Response-Timing"] = duration.ToString();
+                            //Console.WriteLine($"Response end {duration}!!!");
                             return
                                 Task.CompletedTask;
                         }
                     );
             await _next(context);
             stopwatch.Stop();
+            stopwatch = null;
         }
     }
-    public static class RequestResponseMiddlewareExtensions
+    public static class RequestResponseGuardMiddlewareExtensions
     {
         public static IApplicationBuilder UseRequestResponseGuard
             (
                 this IApplicationBuilder target
             )
         {
-            return target.UseMiddleware<RequestResponseGuardMiddleware>();
+            return
+                target
+                    .UseMiddleware
+                        <RequestResponseGuardMiddleware>();
         }
     }
 }
