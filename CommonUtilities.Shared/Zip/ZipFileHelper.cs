37336c8d18a8e59c56fb84a629969bd19ac53f1b
user <user@mail.com>
2018-12-31 23:33:27 +0800

    ZipFileHelper

--- CommonUtilities.Shared/Zip/ZipFileHelper.cs
+++ CommonUtilities.Shared/Zip/ZipFileHelper.cs
@@ -1,10 +1,40 @@
 ﻿namespace Microshaoft
 {
     using System;
+    using System.Collections.Generic;
     using System.IO;
     using System.IO.Compression;
     public static class ZipFileHelper
     {
+        public static void Create
+                    (
+                        string zippedFileName
+                        , IEnumerable<string> originalFiles
+                        , Func<string, string> onCreateEntryProcessFunc//= null
+                    )
+        {
+            using
+                    (
+                        var fileStream = new FileStream
+                                                (
+                                                    zippedFileName
+                                                    , FileMode.OpenOrCreate
+                                                    , FileAccess.Write
+                                                    , FileShare.None
+                                                )
+                    )
+            {
+                using (ZipArchive archive = new ZipArchive(fileStream, ZipArchiveMode.Create))
+                {
+                    foreach (var file in originalFiles)
+                    {
+                        var entryName = onCreateEntryProcessFunc(file);
+                        archive.CreateEntryFromFile(file, entryName);
+                    }
+                    //archive.Dispose();
+                }
+            }
+        }
         public static bool Decompress
                             (
                                 string originalFileFullPath
