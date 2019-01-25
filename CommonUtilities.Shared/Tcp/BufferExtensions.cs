234b578fac9cf7c9f21d6e1f7fe3500e066beebe
user <user@mail.com>
2019-01-25 17:49:06 +0800

    SignalR Socket/TCP

---
+++ CommonUtilities.Shared/Tcp/BufferExtensions.cs
@@ -0,0 +1,23 @@
+﻿#if !NETFRAMEWORK4_X && !XAMARIN
+namespace Microshaoft
+{
+    using System;
+    using System.Runtime.InteropServices;
+    public static class BufferExtensions
+    {
+        public static ArraySegment<byte> GetArray(this Memory<byte> memory)
+        {
+            return ((ReadOnlyMemory<byte>)memory).GetArray();
+        }
+
+        public static ArraySegment<byte> GetArray(this ReadOnlyMemory<byte> memory)
+        {
+            if (!MemoryMarshal.TryGetArray(memory, out var result))
+            {
+                throw new InvalidOperationException("Buffer backed by array was expected");
+            }
+            return result;
+        }
+    }
+}
+#endif
