669d26e2ada7201eaf68d283ce64855b8807f5a4
user <user@mail.com>
2019-02-07 17:31:37 +0800

    IHttpRequestValidateable<JToken>

---
+++ CommonUtilities.Shared/Web.Filters/IParameterValidateable.cs
@@ -0,0 +1,30 @@
+﻿#if NETCOREAPP2_X
+namespace Microshaoft.Web
+{
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.AspNetCore.Mvc.Filters;
+
+    public interface IHttpRequestValidateable<TParameter>
+    {
+        string Name
+        {
+            get;
+        }
+        (
+            bool IsValid
+            , IActionResult Result
+        )
+            Validate
+                (
+                    TParameter parameter
+                    , ActionExecutingContext actionExecutingContext
+                );
+    }
+
+    //public interface IJTokenParameterValidator : IParameterValidateable<JToken>
+    //{
+        
+    //}
+}
+#endif
