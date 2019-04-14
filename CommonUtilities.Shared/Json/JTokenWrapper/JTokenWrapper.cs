9331b2aad16682af85d8459adf30c7061664ce30
user <user@mail.com>
2019-04-15 01:34:21 +0800

    JTokenWrapperIoActivity

---
+++ CommonUtilities.Shared/Json/JTokenWrapper/JTokenWrapper.cs
@@ -0,0 +1,74 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microshaoft
+{
+    public class JTokenWrapper
+    {
+        //private ConcurrentDictionary<string, JTokenWrapper> _properties = new ConcurrentDictionary<string, JTokenWrapper>();
+
+        public JToken Token = null;
+        public JTokenWrapper(JToken jToken)
+        {
+            Token = jToken;
+        }
+        //private JTokenWrapper GetOrAddProperty(string propertyName)
+        //{
+        //    return
+        //        _properties
+        //            .GetOrAdd
+        //                (
+        //                    propertyName
+        //                    , (x) =>
+        //                    {
+        //                        var jToken = _jToken[propertyName];
+        //                        var r = new JTokenWrapper(jToken);
+        //                        return
+        //                            r;
+        //                    }
+        //                );
+        //}
+        //public JToken this[string propertyName]
+        //{
+        //    get
+        //    {
+        //        //var jTokenWrapper = GetOrAddProperty(propertyName);
+        //        return jTokenWrapper;
+        //    }
+        //    set
+        //    {
+        //        var jTokenWrapper = GetOrAddProperty(propertyName);
+        //        jTokenWrapper._jToken[propertyName] = value._jToken;  
+        //    }
+        //}
+        public static JTokenWrapper Parse(string json)
+        {
+            var jToken = JToken.Parse(json);
+            var r = new JTokenWrapper(jToken);
+            return r;
+        }
+        public static T ParseAs<T>(string json)
+                     where T : JContainer
+        {
+            
+            var j = JToken.Parse(json);
+            var r = (T) j;
+            return r;
+        }
+
+        public T TokenAs<T>()
+            where T : JContainer
+        {
+            var r = (T) this.Token;
+            return r;
+        }
+        //public T Value<T>()
+        //{
+        //    return
+        //        _jToken.Value<T>();
+        //}
+    }
+}
---
+++ CommonUtilities.Shared/WorkFlow/CommonJsonIoActivities/AbstractJTokenWrapperIoActivity.cs
@@ -0,0 +1,101 @@
+﻿#if NETFRAMEWORK4_X
+namespace Microshaoft.WorkFlows.Activities
+{
+    using Newtonsoft.Json.Linq;
+    using System;
+    using System.Activities;
+
+    public abstract class AbstractJTokenWrapperIoActivity : NativeActivity<JTokenWrapper>
+    {
+        [RequiredArgument]
+        public InArgument<JTokenWrapper> Inputs { get; set; }
+
+        public abstract JTokenWrapper ExecuteProcess(NativeActivityContext context);
+        
+
+        protected override void Execute(NativeActivityContext context)
+        {
+            JToken parameter = Inputs.Get(context).Token;
+            if (parameter is JArray)
+            {
+
+            }
+            else if (parameter is JObject)
+            {
+                ((JObject)parameter)["F2"] = "aaaaaaaaaaaaaa";
+
+
+                var bookmark = parameter["bookmark"];
+                var jObject = (JObject)parameter;
+                var hasBookmark = jObject
+                                    .TryGetValue
+                                        (
+                                            "bookmark"
+                                            , StringComparison.OrdinalIgnoreCase
+                                            , out var o
+                                        );
+                if (hasBookmark)
+                {
+                    var bookmarkName = o.Value<string>();
+                    context
+                        .CreateBookmark
+                            (
+                                bookmarkName
+                                , new BookmarkCallback
+                                    (
+                                        (x, y, z) =>
+                                        {
+                                            var result = OnResumeBookmarkProcess(x, y);
+                                            Result
+                                                .Set
+                                                    (
+                                                        context
+                                                        , result
+                                                    );
+
+                                        }
+                                    )
+                            );
+                }
+                else
+                {
+                    var result = ExecuteProcess(context);
+                    Result
+                       .Set
+                           (
+                               context
+                               , result
+                           );
+                }
+            }
+            else
+            {
+                throw
+                    new ArgumentException
+                            (
+                                "Argument is not JToken"
+                                , "Inputs"
+                            );
+            }
+        }
+
+        // NativeActivity derived activities that do asynchronous operations by calling 
+        // one of the CreateBookmark overloads defined on System.Activities.NativeActivityContext 
+        // must override the CanInduceIdle property and return true.
+        protected override bool CanInduceIdle
+        {
+            get
+            {
+                return true;
+            }
+        }
+
+        public abstract JTokenWrapper OnResumeBookmarkProcess
+                                    (
+                                        NativeActivityContext context
+                                        , Bookmark bookmark
+                                      //  , object state
+                                    );
+    }
+}
+#endif
