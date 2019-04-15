611508c526aa26dc63e5825ddc5f483582515b6e
user <user@mail.com>
2019-04-15 11:16:29 +0800

    ApprovalStateMachineWorkFlow Test

---
+++ Samples/WorkFlow/ApprovalActivitiesLibrary/ApprovalEntryActivity.cs
@@ -0,0 +1,53 @@
+﻿namespace Microshaoft.WorkFlows.Activities
+{
+    using Newtonsoft.Json.Linq;
+    using System;
+    using System.Activities;
+
+    public sealed class ApprovalEntryActivity : AbstractJTokenWrapperIoActivity
+    {
+        public override JTokenWrapper ExecuteProcess(NativeActivityContext context)
+        {
+            JToken parameter = Inputs.Get(context).Token;
+            JArray steps = null;
+            if (parameter["Steps"] == null)
+            {
+                steps = new JArray();
+                parameter["Steps"] = steps;
+            }
+            else
+            {
+                steps = (JArray)parameter["Steps"];
+            }
+            steps
+                .Add
+                    (
+                        $"Execute:{this.GetType().Name}@{DateTime.Now}"
+                    );
+            var r = new JTokenWrapper(parameter);
+            return r;
+        }
+
+        public override JTokenWrapper OnResumeBookmarkProcess(NativeActivityContext context, Bookmark bookmark)
+        {
+            JToken parameter = Inputs.Get(context).Token;
+            JArray steps = null;
+            if (parameter["Steps"] == null)
+            {
+                steps = new JArray();
+                parameter["Steps"] = steps;
+            }
+            else
+            {
+                steps = (JArray)parameter["Steps"];
+            }
+            steps
+                .Add
+                    (
+                        $"Resume:{this.GetType().Name}@{DateTime.Now}"
+                    );
+            var r = new JTokenWrapper(parameter);
+            return r;
+        }
+    }
+}
