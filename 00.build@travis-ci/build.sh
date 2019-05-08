f62462dd12b1cb8c8a9d9329aa362130d1105bf1
user <user@mail.com>
2019-05-08 22:45:13 +0800

    pwd

--- 00.build@travis-ci/build.sh
+++ 00.build@travis-ci/build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-
+pwd
 set -ex
-
+pwd
 source ./00.build@travis-ci/activate.sh
 
-
-dotnet build CommonUtilities.NET.Core.Standard.sln
+pwd
+dotnet build ./CommonUtilities.NET.Core.Standard.sln
 
 # dotnet test
