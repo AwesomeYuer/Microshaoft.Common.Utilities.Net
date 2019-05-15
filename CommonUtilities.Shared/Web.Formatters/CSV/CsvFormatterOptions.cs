c983f45e19b1a605b6e2aaa032220e6fb15dd670
user <user@mail.com>
2019-05-15 09:54:50 +0800

    csv output

---
+++ CommonUtilities.Shared/Web.Formatters/CSV/CsvFormatterOptions.cs
@@ -0,0 +1,17 @@
+﻿#if NETCOREAPP2_X
+
+namespace Microshaoft.Web
+{
+    using System.Text;
+    public class CsvFormatterOptions
+    {
+        public bool UseSingleLineHeaderInCsv { get; set; } = true;
+
+        public string CsvDelimiter { get; set; } = ";";
+
+        public Encoding Encoding { get; set; } = Encoding.Default;
+
+        public bool IncludeExcelDelimiterHeader { get; set; } = false;
+    }
+}
+#endif
