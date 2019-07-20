fbeddec00bf40b943d206b6066f3dfce9e560b81
user <user@mail.com>
2019-07-20 15:29:58 +0800

    parametersQueryCommandText mssql/mysql/npgsql refact

--- CommonUtilities.Shared/Sql/Microshaoft.00.SqlHelper.cs
+++ CommonUtilities.Shared/Sql/Microshaoft.00.SqlHelper.cs
@@ -72,8 +72,8 @@
             return clone;
         }
 
-        public static 
-                IEnumerable<TDbParameter> 
+        public static
+                IEnumerable<TDbParameter>
                     GetStoreProcedureDefinitionParameters
                         <TDbConnection, TDbCommand, TDbParameter>
                             (
@@ -85,6 +85,7 @@
                                         onQueryDefinitionsSetReturnParameterProcessFunc
                                 , Func<IDataReader, TDbParameter, string, TDbParameter>
                                         onQueryDefinitionsReadOneDbParameterProcessFunc
+                                , string parametersQueryCommandText //= null
                                 , bool includeReturnValueParameter = false
                             )
                     where
@@ -105,25 +106,26 @@
                 var dataBase = connection.Database;
                 string procedureSchema = string.Empty;
                 string parameterName = string.Empty;
-                string parametersSchema = "information_schema.parameters";
-                if (connection is SqlConnection)
-                {
-                    parametersSchema = MsSqlStoreProceduresParametersSchemaQuery;
-                }
-                var commandText = $@"
-                    SELECT
-                        * 
-                    FROM
-                        {parametersSchema} a 
-                    WHERE
-                        a.SPECIFIC_NAME = @ProcedureName
-                    ";
+                //string parametersSchema = "information_schema.parameters";
+                //if (connection is SqlConnection)
+                //{
+                //    parametersSchema = MsSqlStoreProceduresParametersSchemaQuery;
+                //}
+                //var commandText = $@"
+                //    SELECT
+                //        top 1
+                //        * 
+                //    FROM
+                //        {parametersSchema} a 
+                //    WHERE
+                //        a.SPECIFIC_NAME like @ProcedureName + '%'
+                //    ";
                 //MySQL 不支持 using command
                 //using
                 //    (
                 DbCommand command = new TDbCommand()
                 {
-                    CommandText = commandText
+                    CommandText = parametersQueryCommandText
                     , CommandType = CommandType.Text
                     , Connection = connection
                 };
@@ -161,6 +163,8 @@
                                     dbParameterReturn
                                 );
                     connection.Open();
+                    //Npgsql should be use
+                    //OpenAsync().Wait();
                     var dataReader
                             = command
                                     .ExecuteReader
@@ -243,196 +247,5 @@
             return pd;
         }
     }
-    public static partial class SqlHelper
-    {
-        private const string MsSqlStoreProceduresParametersSchemaQuery =
-@"
-(
-    SELECT
 
-         DB_NAME()                                  AS SPECIFIC_CATALOG
-        , SCHEMA_NAME(o.schema_id)                  AS SPECIFIC_SCHEMA
-        , o.name                                    AS SPECIFIC_NAME
-        , c.parameter_id                            AS ORDINAL_POSITION
-        ,
-            convert
-                (
-                    nvarchar(10)
-                    ,
-                        CASE
-                            WHEN
-                                c.parameter_id = 0
-                                    THEN
-                                        'OUT'
-                            WHEN
-                                c.is_output = 1
-                                    THEN
-                                        'INOUT'
-                            ELSE
-                                'IN'
-                        END
-                )                                   AS PARAMETER_MODE
-        ,
-            convert
-                (
-                    nvarchar(10)
-                    ,
-                        CASE
-                            WHEN
-                                c.parameter_id = 0
-                                    THEN
-                                        'YES'
-                            ELSE
-                                'NO'
-                        END
-                )                                           AS IS_RESULT
-        , convert(nvarchar(10), 'NO')                       AS AS_LOCATOR
-        , c.name                                            AS PARAMETER_NAME
-        , ISNULL(TYPE_NAME(c.system_type_id), u.name)       AS DATA_TYPE
-        , COLUMNPROPERTY
-            (c.object_id, c.name, 'charmaxlen')             AS CHARACTER_MAXIMUM_LENGTH
-        , COLUMNPROPERTY
-            (c.object_id, c.name, 'octetmaxlen')            AS CHARACTER_OCTET_LENGTH
-        , convert(sysname, null)                            AS COLLATION_CATALOG
-        , convert(sysname, null) collate catalog_default    AS COLLATION_SCHEMA
-        ,
-            convert
-                (
-                    sysname
-                    ,
-                        CASE
-                            WHEN
-                                c.system_type_id IN(35, 99, 167, 175, 231, 239)
-                                    THEN--[n]char /[n]varchar /[n]text
-                                        SERVERPROPERTY('collation')
-                        END
-                )                                              AS COLLATION_NAME
-        , convert(sysname, null)                               AS CHARACTER_SET_CATALOG
-        , convert(sysname, null) collate catalog_default       AS CHARACTER_SET_SCHEMA
-        ,
-            convert
-                (
-                    sysname
-                    ,
-                        CASE
-                            WHEN
-                                c.system_type_id IN(35, 167, 175)
-                                    THEN
-                                        SERVERPROPERTY('sqlcharsetname')-- char / varchar / text
-                            WHEN
-                                c.system_type_id IN(99, 231, 239)
-                                    THEN
-                                        N'UNICODE'-- nchar / nvarchar / ntext
-                        END
-                )                                               AS CHARACTER_SET_NAME
-        ,
-            convert
-                (
-                    tinyint
-                    ,
-                        CASE-- int / decimal / numeric / real / float / money
-                            WHEN
-                                c.system_type_id IN(48, 52, 56, 59, 60, 62, 106, 108, 122, 127)
-                                    THEN
-                                        c.precision
-                        END
-                )                                               AS NUMERIC_PRECISION
-        ,
-            convert
-                (
-                    smallint
-                        ,
-                            CASE-- int / money / decimal / numeric
-                                WHEN
-                                    c.system_type_id IN(48, 52, 56, 60, 106, 108, 122, 127)
-                                        THEN
-                                            10
-                                WHEN
-                                    c.system_type_id IN(59, 62)
-                                        THEN
-                                            2
-                            END
-                )                                               AS NUMERIC_PRECISION_RADIX
-        , --real / float
-            convert
-                (
-                    int
-                    ,
-                        CASE-- datetime / smalldatetime
-                            WHEN
-                                c.system_type_id IN(40, 41, 42, 43, 58, 61)
-                                    THEN
-                                        NULL
-                            ELSE
-                                ODBCSCALE(c.system_type_id, c.scale)
-                        END
-                )                                               AS NUMERIC_SCALE
-        ,
-            convert
-                (
-                    smallint
-                    ,
-                        CASE-- datetime / smalldatetime
-                            WHEN
-                                c.system_type_id IN(40, 41, 42, 43, 58, 61)
-                                    THEN
-                                        ODBCSCALE(c.system_type_id, c.scale)
-                        END
-                )                                               AS DATETIME_PRECISION
-        , convert(nvarchar(30), null)                           AS INTERVAL_TYPE
-        , convert(smallint, null)                               AS INTERVAL_PRECISION
-        ,
-            convert
-                (
-                    sysname
-                    ,
-                        CASE
-                            WHEN
-                                u.schema_id <> 4
-                                    THEN
-                                        DB_NAME()
-                        END
-                )                                               AS USER_DEFINED_TYPE_CATALOG
-        ,
-            convert
-                (
-                    sysname
-                    ,
-                        CASE
-                            WHEN
-                                u.schema_id <> 4
-                                    THEN
-                                        SCHEMA_NAME(u.schema_id)
-                        END
-                )                                               AS USER_DEFINED_TYPE_SCHEMA
-        ,
-            convert
-                (
-                    sysname
-                    ,
-                        CASE
-                            WHEN
-                                u.schema_id <> 4
-                                    THEN
-                                        u.name
-                            END
-                )                                               AS USER_DEFINED_TYPE_NAME
-        , convert(sysname, null)                                AS SCOPE_CATALOG
-        , convert(sysname, null) collate catalog_default        AS SCOPE_SCHEMA
-        , convert(sysname, null) collate catalog_default        AS SCOPE_NAME
-    FROM
-        sys.all_objects o
-            JOIN
-                sys.all_parameters c
-                    ON
-                        c.object_id = o.object_id
-            JOIN
-                sys.types u
-                    ON
-                        u.user_type_id = c.user_type_id
-    WHERE
-        o.type IN('P', 'FN', 'TF', 'IF', 'IS', 'AF', 'PC', 'FS', 'FT')
-)
-        ";
-    }
 }
