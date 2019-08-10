7981e1aeb4884b21ff8b75cd5a1ff4ca8f5f2ecd
user <user@mail.com>
2019-08-11 00:53:52 +0800

    mkdir -p Samples/MsSqlCodeDiffVersioning/bin/Debug/netcoreapp2.2/wwwroot/

--- copyrundebug.sh
+++ copyrundebug.sh
@@ -4,8 +4,10 @@ cp StoreProcedureWebApiExecutorsPlugins/MsSQL.StoreProcedureWebApiExecutor.Plugi
 cp StoreProcedureWebApiExecutorsPlugins/MySQL.StoreProcedureWebApiExecutor.Plugin/bin/Debug/netcoreapp2.2/*Plugin*  Samples/MsSqlCodeDiffVersioning/bin/Debug/netcoreapp2.2/CompositionPlugins/
 cp JTokenModelParameterValidatorsPlugins/JTokenModelParameterValidatorSamplePlugin/bin/Debug/netcoreapp2.2/*Plugin* Samples/MsSqlCodeDiffVersioning/bin/Debug/netcoreapp2.2/CompositionPlugins/
 
+mkdir -p Samples/MsSqlCodeDiffVersioning/bin/Debug/netcoreapp2.2/wwwroot/
 cp -rf Samples/MsSqlCodeDiffVersioning/wwwroot/* Samples/MsSqlCodeDiffVersioning/bin/Debug/netcoreapp2.2/wwwroot/
 
+mkdir -p Samples/MsSqlCodeDiffVersioning/bin/Debug/netcoreapp2.2/CompositionPlugins/
 cp StoreProcedureWebApiExecutorsPlugins/MySQL.StoreProcedureWebApiExecutor.Plugin/bin/Debug/netcoreapp2.2/*MySql*   Samples/MsSqlCodeDiffVersioning/bin/Debug/netcoreapp2.2/CompositionPlugins/
 cp StoreProcedureWebApiExecutorsPlugins/MySQL.StoreProcedureWebApiExecutor.Plugin/bin/Debug/netcoreapp2.2/*Npgsql*  Samples/MsSqlCodeDiffVersioning/bin/Debug/netcoreapp2.2/CompositionPlugins/
 cp StoreProcedureWebApiExecutorsPlugins/MySQL.StoreProcedureWebApiExecutor.Plugin/bin/Debug/netcoreapp2.2/*Sqlite*  Samples/MsSqlCodeDiffVersioning/bin/Debug/netcoreapp2.2/CompositionPlugins/
