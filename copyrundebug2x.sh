597dd40b023ab601609bdc10627855e91f2f72df
user <user@mail.com>
2019-10-01 19:36:05 +0800

    copyrundebug3x.sh

---
+++ copyrundebug2x.sh
@@ -0,0 +1,26 @@
+dotnet build CommonUtilities.NET.Core.Standard.sln -c Debug
+
+mkdir -p Samples/MsSqlCodeDiffVersioning/bin/Debug/netcoreapp2.2/wwwroot/
+cp -rf Samples/MsSqlCodeDiffVersioning/wwwroot/* Samples/MsSqlCodeDiffVersioning/bin/Debug/netcoreapp2.2/wwwroot/
+
+mkdir -p Samples/MsSqlCodeDiffVersioning/bin/Debug/netcoreapp2.2/CompositionPlugins/
+cp StoreProcedureWebApiExecutorsPlugins/MsSQL.Plugin/MsSQL.Plugin.2.x/bin/Debug/netcoreapp2.2/*Plugin*  Samples/MsSqlCodeDiffVersioning/bin/Debug/netcoreapp2.2/CompositionPlugins/
+cp StoreProcedureWebApiExecutorsPlugins/MsSQL.Plugin/MsSQL.Plugin.2.x/bin/Debug/netcoreapp2.2/*Plugin*  Samples/MsSqlCodeDiffVersioning/bin/Debug/netcoreapp2.2/CompositionPlugins/
+cp JTokenModelParameterValidatorsPlugins/SamplePlugin/SamplePlugin.2.x/bin/Debug/netcoreapp2.2/*Plugin* Samples/MsSqlCodeDiffVersioning/bin/Debug/netcoreapp2.2/CompositionPlugins/
+
+cp StoreProcedureWebApiExecutorsPlugins/MySQL.Plugin/MsSQL.Plugin.2.x/bin/Debug/netcoreapp2.2/*MySql*   Samples/MsSqlCodeDiffVersioning/bin/Debug/netcoreapp2.2/CompositionPlugins/
+cp StoreProcedureWebApiExecutorsPlugins/MySQL.Plugin/MsSQL.Plugin.2.x/bin/Debug/netcoreapp2.2/*Npgsql*  Samples/MsSqlCodeDiffVersioning/bin/Debug/netcoreapp2.2/CompositionPlugins/
+cp StoreProcedureWebApiExecutorsPlugins/MySQL.Plugin/MsSQL.Plugin.2.x/bin/Debug/netcoreapp2.2/*Sqlite*  Samples/MsSqlCodeDiffVersioning/bin/Debug/netcoreapp2.2/CompositionPlugins/
+cp StoreProcedureWebApiExecutorsPlugins/MySQL.Plugin/MsSQL.Plugin.2.x/bin/Debug/netcoreapp2.2/*SQLite*  Samples/MsSqlCodeDiffVersioning/bin/Debug/netcoreapp2.2/CompositionPlugins/
+cp StoreProcedureWebApiExecutorsPlugins/MySQL.Plugin/MsSQL.Plugin.2.x/bin/Debug/netcoreapp2.2/*Oracle*  Samples/MsSqlCodeDiffVersioning/bin/Debug/netcoreapp2.2/CompositionPlugins/
+cp StoreProcedureWebApiExecutorsPlugins/MySQL.Plugin/MsSQL.Plugin.2.x/bin/Debug/netcoreapp2.2/*DB2*     Samples/MsSqlCodeDiffVersioning/bin/Debug/netcoreapp2.2/CompositionPlugins/
+
+cp StoreProcedureWebApiExecutorsPlugins/MySQL.Plugin/MsSQL.Plugin.2.x/bin/Debug/netcoreapp2.2/*MySql*   Samples/MsSqlCodeDiffVersioning/bin/Debug/netcoreapp2.2/
+cp StoreProcedureWebApiExecutorsPlugins/MySQL.Plugin/MsSQL.Plugin.2.x/bin/Debug/netcoreapp2.2/*Npgsql*  Samples/MsSqlCodeDiffVersioning/bin/Debug/netcoreapp2.2/
+cp StoreProcedureWebApiExecutorsPlugins/MySQL.Plugin/MsSQL.Plugin.2.x/bin/Debug/netcoreapp2.2/*Sqlite*  Samples/MsSqlCodeDiffVersioning/bin/Debug/netcoreapp2.2/
+cp StoreProcedureWebApiExecutorsPlugins/MySQL.Plugin/MsSQL.Plugin.2.x/bin/Debug/netcoreapp2.2/*SQLite*  Samples/MsSqlCodeDiffVersioning/bin/Debug/netcoreapp2.2/
+cp StoreProcedureWebApiExecutorsPlugins/MySQL.Plugin/MsSQL.Plugin.2.x/bin/Debug/netcoreapp2.2/*Oracle*  Samples/MsSqlCodeDiffVersioning/bin/Debug/netcoreapp2.2/
+cp StoreProcedureWebApiExecutorsPlugins/MySQL.Plugin/MsSQL.Plugin.2.x/bin/Debug/netcoreapp2.2/*DB2*     Samples/MsSqlCodeDiffVersioning/bin/Debug/netcoreapp2.2/
+
+cd Samples/MsSqlCodeDiffVersioning/bin/Debug/netcoreapp2.2/
+dotnet MsSqlCodeDiffVersioning.WebApplication.dll $1
---
+++ copyrundebug3x.sh
@@ -0,0 +1,26 @@
+dotnet build CommonUtilities.NET.Core.Standard.3.0.sln -c Debug
+
+mkdir -p Samples/MsSqlCodeDiffVersioning/bin/Debug/netcoreapp3.0/wwwroot/
+cp -rf Samples/MsSqlCodeDiffVersioning/wwwroot/* Samples/MsSqlCodeDiffVersioning/bin/Debug/netcoreapp3.0/wwwroot/
+
+mkdir -p Samples/MsSqlCodeDiffVersioning/bin/Debug/netcoreapp3.0/CompositionPlugins/
+cp StoreProcedureWebApiExecutorsPlugins/MsSQL.Plugin/MsSQL.Plugin.3.x/bin/Debug/netcoreapp3.0/*Plugin*  Samples/MsSqlCodeDiffVersioning/bin/Debug/netcoreapp3.0/CompositionPlugins/
+cp StoreProcedureWebApiExecutorsPlugins/MsSQL.Plugin/MsSQL.Plugin.3.x/bin/Debug/netcoreapp3.0/*Plugin*  Samples/MsSqlCodeDiffVersioning/bin/Debug/netcoreapp3.0/CompositionPlugins/
+cp JTokenModelParameterValidatorsPlugins/SamplePlugin/SamplePlugin.3.x/bin/Debug/netcoreapp3.0/*Plugin* Samples/MsSqlCodeDiffVersioning/bin/Debug/netcoreapp3.0/CompositionPlugins/
+
+cp StoreProcedureWebApiExecutorsPlugins/MySQL.Plugin/MsSQL.Plugin.3.x/bin/Debug/netcoreapp3.0/*MySql*   Samples/MsSqlCodeDiffVersioning/bin/Debug/netcoreapp3.0/CompositionPlugins/
+cp StoreProcedureWebApiExecutorsPlugins/MySQL.Plugin/MsSQL.Plugin.3.x/bin/Debug/netcoreapp3.0/*Npgsql*  Samples/MsSqlCodeDiffVersioning/bin/Debug/netcoreapp3.0/CompositionPlugins/
+cp StoreProcedureWebApiExecutorsPlugins/MySQL.Plugin/MsSQL.Plugin.3.x/bin/Debug/netcoreapp3.0/*Sqlite*  Samples/MsSqlCodeDiffVersioning/bin/Debug/netcoreapp3.0/CompositionPlugins/
+cp StoreProcedureWebApiExecutorsPlugins/MySQL.Plugin/MsSQL.Plugin.3.x/bin/Debug/netcoreapp3.0/*SQLite*  Samples/MsSqlCodeDiffVersioning/bin/Debug/netcoreapp3.0/CompositionPlugins/
+cp StoreProcedureWebApiExecutorsPlugins/MySQL.Plugin/MsSQL.Plugin.3.x/bin/Debug/netcoreapp3.0/*Oracle*  Samples/MsSqlCodeDiffVersioning/bin/Debug/netcoreapp3.0/CompositionPlugins/
+cp StoreProcedureWebApiExecutorsPlugins/MySQL.Plugin/MsSQL.Plugin.3.x/bin/Debug/netcoreapp3.0/*DB2*     Samples/MsSqlCodeDiffVersioning/bin/Debug/netcoreapp3.0/CompositionPlugins/
+
+cp StoreProcedureWebApiExecutorsPlugins/MySQL.Plugin/MsSQL.Plugin.3.x/bin/Debug/netcoreapp3.0/*MySql*   Samples/MsSqlCodeDiffVersioning/bin/Debug/netcoreapp3.0/
+cp StoreProcedureWebApiExecutorsPlugins/MySQL.Plugin/MsSQL.Plugin.3.x/bin/Debug/netcoreapp3.0/*Npgsql*  Samples/MsSqlCodeDiffVersioning/bin/Debug/netcoreapp3.0/
+cp StoreProcedureWebApiExecutorsPlugins/MySQL.Plugin/MsSQL.Plugin.3.x/bin/Debug/netcoreapp3.0/*Sqlite*  Samples/MsSqlCodeDiffVersioning/bin/Debug/netcoreapp3.0/
+cp StoreProcedureWebApiExecutorsPlugins/MySQL.Plugin/MsSQL.Plugin.3.x/bin/Debug/netcoreapp3.0/*SQLite*  Samples/MsSqlCodeDiffVersioning/bin/Debug/netcoreapp3.0/
+cp StoreProcedureWebApiExecutorsPlugins/MySQL.Plugin/MsSQL.Plugin.3.x/bin/Debug/netcoreapp3.0/*Oracle*  Samples/MsSqlCodeDiffVersioning/bin/Debug/netcoreapp3.0/
+cp StoreProcedureWebApiExecutorsPlugins/MySQL.Plugin/MsSQL.Plugin.3.x/bin/Debug/netcoreapp3.0/*DB2*     Samples/MsSqlCodeDiffVersioning/bin/Debug/netcoreapp3.0/
+
+cd Samples/MsSqlCodeDiffVersioning/bin/Debug/netcoreapp3.0/
+dotnet MsSqlCodeDiffVersioning.WebApplication.dll $1
