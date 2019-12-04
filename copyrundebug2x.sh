e848a6d945bf301042a0df9fa776c13bcb798175
user <user@mail.com>
2019-12-04 16:01:09 +0800

    upgrade some nuget and fix copyrundebugX.x.sh wwwwroot source path/encoding/LF

--- copyrundebug2x.sh
+++ copyrundebug2x.sh
@@ -1,7 +1,7 @@
-dotnet build CommonUtilities.NET.Core.Standard.sln -c Debug
+﻿dotnet build CommonUtilities.NET.Core.Standard.sln -c Debug
 
 mkdir -p Samples/MsSqlCodeDiffVersioning/MsSqlCodeDiffVersioning.2x/bin/Debug/netcoreapp2.2/wwwroot/
-cp -rf Samples/MsSqlCodeDiffVersioning/wwwroot/* Samples/MsSqlCodeDiffVersioning/MsSqlCodeDiffVersioning.2x/bin/Debug/netcoreapp2.2/wwwroot/
+cp -rf Samples/MsSqlCodeDiffVersioning/MsSqlCodeDiffVersioning.Shared/wwwroot/* Samples/MsSqlCodeDiffVersioning/MsSqlCodeDiffVersioning.2x/bin/Debug/netcoreapp2.2/wwwroot/
 
 mkdir -p Samples/MsSqlCodeDiffVersioning/MsSqlCodeDiffVersioning.2x/bin/Debug/netcoreapp2.2/CompositionPlugins/
 cp StoreProcedureWebApiExecutorsPlugins/MsSQL.Plugin/MsSQL.Plugin.2.x/bin/Debug/netcoreapp2.2/*Plugin*  Samples/MsSqlCodeDiffVersioning/MsSqlCodeDiffVersioning.2x/bin/Debug/netcoreapp2.2/CompositionPlugins/
