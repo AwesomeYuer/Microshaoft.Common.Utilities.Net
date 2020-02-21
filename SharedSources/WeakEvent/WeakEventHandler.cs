c0dade975fe0a5731b12f5b4438c94c1a32c8e36
user <user@mail.com>
2020-02-22 00:53:22 +0800

    WeakEvent Manager/Listener/Handler, ConsoleTables, AssemblyResolver

---
+++ SharedSources/WeakEvent/WeakEventHandler.cs
@@ -0,0 +1,34 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+namespace Microshaoft
+{
+    using System;
+    using System.Reflection;
+
+    public sealed class WeakEventHandler<TEventArgs> where TEventArgs : EventArgs
+    {
+        private readonly WeakReference _targetWeakReference;
+        private readonly MethodInfo _method;
+
+        public WeakEventHandler(EventHandler<TEventArgs> callback)
+        {
+            _method = callback.Method;
+            _targetWeakReference = new WeakReference(callback.Target, true);
+        }
+
+        //[DebuggerNonUserCode]
+        public void Handler(object sender, TEventArgs e)
+        {
+            var target = _targetWeakReference.Target;
+            if (target != null)
+            {
+                var callback = (Action<object, TEventArgs>)Delegate.CreateDelegate(typeof(Action<object, TEventArgs>), target, _method, true);
+                if (callback != null)
+                {
+                    callback(sender, e);
+                }
+            }
+        }
+    }
+}
